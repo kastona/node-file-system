6c87182940403caddfa69f7e517c0d533695abef
Stephen Kastona <stephenkastona@gmail.com>
2020-01-11 10:15:35 +0100

    included chalk module

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const getNotes = require('./app-files/notes');
+const getEmail = require('./app-files/notes');
+const validator = require('validator')
+const chalk = require('chalk')
 
 
-console.log(getNotes());
+const email = 'stephenkastonagmail.com'
+
+if(validator.isEmail(email)) {
+    console.log(chalk.green('you entered a correct email!'))
+} else {
+    console.log(chalk.red('This is not an email!'))
+}
