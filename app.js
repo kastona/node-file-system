5ba6f02b9a2b37d4ea97aec1e74cb8c1756ace60
Stephen Kastona <stephenkastona@gmail.com>
2020-01-11 16:51:26 +0100

    setup yargs to require the right data for add method

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 
-const argument = process.argv[2]
+const yargs = require('yargs')
 
-if(argument === 'add') {
-    console.log('do some addition here')
-} else  if(argument === 'subtract') {
-    console.log('do some subtraction here')
-} else {
-    console.log('I can\'t do anything here')
-}
+yargs.command({
+    command: 'add',
+    describe: 'adds a note',
+    builder: {
+        title: {
+            describe: 'null title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'holds body of note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log(`Adding note with title ${argv.title}`);
+        console.log(`Body: ${argv.body}`)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    handler: function () {
+        console.log('removing a note')
+    }
+})
+
+yargs.command({
+    command: 'edit',
+    describe: 'edits a note',
+    handler: function() {
+        console.log('editing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'shows a list of notes',
+    handler: function() {
+        console.log('listing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'reads notes out loud',
+    handler: function() {
+        console.log('reading notes')
+    }
+})
+
+
+yargs.parse()
